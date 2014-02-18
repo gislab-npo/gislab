5ffa04d5335b36bdb8360949b9a383789574ea33
Ivan Mincik <ivan.mincik@gmail.com>
2014-02-18 16:07:27 +0100

    Fixed 'SITEURL'.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://imincik.github.io/gis-lab'
+SITEURL = 'http://imincik.github.io/testpelican'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
