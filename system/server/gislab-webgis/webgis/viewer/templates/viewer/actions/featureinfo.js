ff534a03194edeead6ea827481ef8dcd1a7def08
Marcel Dancak <marcel.dancak@gista.sk>
2014-03-11 15:21:17 +0100

    webgis: added combobox with avaulable layers into featureinfo tool

--- system/server/gislab-webgis/webgis/viewer/templates/viewer/actions/featureinfo.js
+++ system/server/gislab-webgis/webgis/viewer/templates/viewer/actions/featureinfo.js
@@ -1,17 +1,59 @@
 
 // Featureinfo Action
+var layers_store_data = {
+	layers: [
+		{ name: 'All visible layers' },
+		{% for layer in layers %}
+		{ name: '{{ layer.name }}' },
+		{% endfor %}
+	]
+};
+
+var identify_layer_combobox = new Ext.form.ComboBox({
+	width: 150,
+	mode: 'local',
+	triggerAction: 'all',
+	forceSelection: true,
+	store: new Ext.data.JsonStore({
+		data: layers_store_data,
+		storeId: 'search-layer-store',
+		root: 'layers',
+		fields: [{
+			name: 'name',
+			type: 'string'
+		}]
+	}),
+	valueField: 'name',
+	displayField: 'name',
+	listeners: {
+		afterrender: function(combo) {
+			if (combo.getStore().getCount() > 0) {
+				var recordSelected = combo.getStore().getAt(0);
+				combo.setValue(recordSelected.get('name'));
+			}
+		}
+	}
+});
+
 ctrl = new OpenLayers.Control.WMSGetFeatureInfo({
 	url: '{{ getfeatureinfo_url }}',
 	autoActivate: false,
 	infoFormat: 'application/vnd.ogc.gml',
 	maxFeatures: 10,
-	queryVisible: true,
 	eventListeners: {
 		beforegetfeatureinfo: function(event) {
 			// because we provided 'url' parameter that will not match url of any layer,
 			// GetFeatureInfo request will not be performed automatically, so we have to
 			// create it manualy with given 'url'.
+			var visible_layers = overlays_group_layer.params.LAYERS; // save LAYERS parameter value
+			if (identify_layer_combobox.selectedIndex > 0) {
+				this.queryVisible = false;
+				overlays_group_layer.params.LAYERS = [identify_layer_combobox.getValue()];
+			} else {
+				this.queryVisible = true;
+			}
 			var wms_options = this.buildWMSOptions(this.url, [overlays_group_layer], event.xy);
+			overlays_group_layer.params.LAYERS = visible_layers; // restore LAYERS parameter value
 			var request = OpenLayers.Request.GET(wms_options);
 		},
 		getfeatureinfo: function(e) {
@@ -29,4 +71,4 @@ action = new GeoExt.Action({
 	group: 'tools',
 	tooltip: 'Feature info'
 })
-mappanel.getTopToolbar().add('-', action);
+mappanel.getTopToolbar().add(identify_layer_combobox, '-', action);
