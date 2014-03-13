8aa687bc9db0c5f8e61df0ea8330599242813dde
Marcel Dancak <marcel.dancak@gista.sk>
2014-03-13 13:15:06 +0100

    webgis: toggle button state control window of history of saved drawings

--- system/server/gislab-webgis/webgis/viewer/templates/viewer/actions/save_history.js
+++ system/server/gislab-webgis/webgis/viewer/templates/viewer/actions/save_history.js
@@ -5,6 +5,7 @@ var action = new Ext.Action({
 	cls: 'x-btn-icon',
 	iconCls: 'save-history-icon',
 	tooltip: 'History of saved drawings',
+	toggleGroup: 'tools',
 	store: new Ext.data.ArrayStore({
 		fields: [
 			{name: 'title', type: 'string'},
@@ -14,69 +15,73 @@ var action = new Ext.Action({
 		]
 	}),
 
-	handler: function(action) {
-		// manually load local data
-		//store.loadData(myData);
+	toggleHandler: function(action, toggled) {
+		if (toggled) {
+			// create the Grid
+			var grid = new Ext.grid.GridPanel({
+				id: 'save-history-grid',
+				store: this.store,
+				viewConfig: {
+					templates: {
+						cell: new Ext.Template(
+							'<td class="x-grid3-col x-grid3-cell x-grid3-td-{id} x-selectable {css}" style="{style}" tabIndex="0" {cellAttr}>\
+								<div class="x-grid3-cell-inner x-grid3-col-{id}" {attr}>{value}</div>\
+							</td>'
+						)
+					}
+				},
+				columns: [
+					{
+						id       : 'title',
+						header   : 'Title',
+						sortable : false,
+						dataIndex: 'title',
+					},
+					{
+						id       : 'time',
+						header   : 'Time',
+						width    : 60,
+						sortable : false,
+						dataIndex: 'time',
+						renderer : Ext.util.Format.dateRenderer('H:i:s'),
+					},
+					{
+						id       : 'link',
+						header   : 'GeoJSON',
+						width    : 75,
+						sortable : false,
+						dataIndex: 'link',
+					},
+					{
+						id       : 'info',
+						header   : 'Info',
+						width    : 180,
+						sortable : false,
+						dataIndex: 'info'
+					},
+				],
+				//stripeRows: true,
+				autoExpandColumn: 'title',
+				// config options for stateful behavior
+				stateful: true,
+				stateId: 'grid'
+			});
 
-		// create the Grid
-		var grid = new Ext.grid.GridPanel({
-			id: 'save-history-grid',
-			store: this.store,
-			viewConfig: {
-				templates: {
-					cell: new Ext.Template(
-						'<td class="x-grid3-col x-grid3-cell x-grid3-td-{id} x-selectable {css}" style="{style}" tabIndex="0" {cellAttr}>\
-							<div class="x-grid3-cell-inner x-grid3-col-{id}" {attr}>{value}</div>\
-						</td>'
-					)
-				}
-			},
-			columns: [
-				{
-					id       : 'title',
-					header   : 'Title',
-					sortable : false,
-					dataIndex: 'title',
-				},
-				{
-					id       : 'time',
-					header   : 'Time',
-					width    : 60, 
-					sortable : false,
-					dataIndex: 'time',
-					renderer : Ext.util.Format.dateRenderer('H:i:s'),
-				},
-				{
-					id       : 'link',
-					header   : 'GeoJSON',
-					width    : 75,
-					sortable : false,
-					dataIndex: 'link',
-				},
-				{
-					id       : 'info',
-					header   : 'Info',
-					width    : 180,
-					sortable : false,
-					dataIndex: 'info'
-				},
-			],
-			//stripeRows: true,
-			autoExpandColumn: 'title',
-			// config options for stateful behavior
-			stateful: true,
-			stateId: 'grid'
-		});
-
-		var history_window = new Ext.Window({
-			title: 'History of saved drawings',
-			width: 500,
-			height: 400,
-			layout: 'fit',
-			items: [grid]
-		});
-		history_window.show();
-		//history_window.alignTo(mappanel.getTopToolbar().getId(), 'bl', [80, 40]);
+			var history_window = new Ext.Window({
+				header: false,
+				closable: false,
+				width: 500,
+				height: 400,
+				layout: 'fit',
+				items: [grid]
+			});
+			history_window.show();
+			history_window.alignTo(Ext.getBody(), 'r-r', [-20, 0]);
+			action.window = history_window;
+		} else {
+			action.window.destroy();
+			return;
+		}
 	}
 });
 mappanel.getTopToolbar().add(action);
