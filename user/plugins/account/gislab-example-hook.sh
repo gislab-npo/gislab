546b3de5c665da895b49e31a05d4076564ac3449
Ivan Mincik <ivan.mincik@gmail.com>
2014-05-10 16:22:29 +0200

    plugins: moving plugins to separate repository (https://github.com/imincik/gis-lab-plugins).

--- user/plugins/account/gislab-example-hook.sh
+++ user/plugins/account/gislab-example-hook.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Example GIS.lab account hook
 
-echo "This is an example account plugin."
 
+echo "This is an example account hook."
 case $GISLAB_ACCOUNT_ACTION in
 	add)
 		echo "Adding user '$GISLAB_USER'."
@@ -13,6 +14,5 @@ case $GISLAB_ACCOUNT_ACTION in
 		echo "Unknown action."
 		;;
 esac
-
 
 # vim: set ts=4 sts=4 sw=4 noet:
--- user/plugins/server/gislab-example-plugin.sh
+++ user/plugins/server/gislab-example-plugin.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+# Example GIS.lab server plugin
 
-echo "This is an example server plugin."
 
+echo "Running example GIS.lab server plugin."
 case $GISLAB_INSTALL_ACTION in
 	install)
-		echo "Performing initial GIS.lab installation."
+		echo "Performing initial GIS.lab server installation."
 		;;
 	upgrade)
-		echo "Performing GIS.lab upgrade."
+		echo "Performing GIS.lab server upgrade."
 		;;
 	*)
 		echo "Unknown action."
 		;;
 esac
-
 
 # vim: set ts=4 sts=4 sw=4 noet:
