19ea5bed378fb2ce6456a9ceb5c97347d9c7130c
Ivan Mincik <ivan.mincik@gmail.com>
2014-07-08 18:38:56 +0200

    install: moving provider information from 'installation-done' script to separate file.

---
+++ system/server/999-installation-done/install-aws.sh
@@ -0,0 +1,14 @@
+# AWS provider
+
+
+installation_summary="
+\nAMAZON WEB SERVICES"
+\n==================="
+\n* Instance ID        : $GISLAB_SERVER_AWS_INSTANCE_ID
+\n* Public IP          : $GISLAB_SERVER_AWS_PUBLIC_IP
+\n* Public hostname    : $GISLAB_SERVER_AWS_PUBLIC_HOSTNAME
+"
+echo -e $installation_summary
+
+
+# vim: set syntax=sh ts=4 sts=4 sw=4 noet:
--- system/server/999-installation-done/install.sh
+++ system/server/999-installation-done/install.sh
@@ -8,31 +8,23 @@ mkdir -p /etc/gislab
 echo "$(gislab_config_header)" >> /etc/gislab/installation.done
 
 
+# print installation summary
+gislab_print_info "Installation finished"
+
 installation_summary="
-GIS.lab is installed and ready to use!
-\n
 \nGENERAL INFORMATION
 \n===================
 \n* GIS.lab ID        : $GISLAB_UNIQUE_ID
 \n* GIS.lab version   : $GISLAB_VERSION
 \n
 "
-
-# append AWS information if running on AWS
-if [ "$GISLAB_SERVER_PROVIDER" == "aws" ]; then
-	installation_summary+="\nAMAZON WEB SERVICES"
-	installation_summary+="\n==================="
-	installation_summary+="\n* Instance ID        : $GISLAB_SERVER_AWS_INSTANCE_ID"
-	installation_summary+="\n* Public IP          : $GISLAB_SERVER_AWS_PUBLIC_IP"
-	installation_summary+="\n* Public hostname    : $GISLAB_SERVER_AWS_PUBLIC_HOSTNAME"
-fi
-
-# print installation summary
-gislab_print_info "Installation finished"
 echo -e $installation_summary
 
-# send installation summary to administrator by email
-echo -e $installation_summary | mail -s "[GIS.lab ID: $GISLAB_UNIQUE_ID] Installation done" root
+
+# load provider customization file if exists
+if [ -f "$GISLAB_INSTALL_CURRENT_ROOT/install-$GISLAB_SERVER_PROVIDER.sh" ]; then
+	source $GISLAB_INSTALL_CURRENT_ROOT/install-$GISLAB_SERVER_PROVIDER.sh
+fi
 
 
 # vim: set syntax=sh ts=4 sts=4 sw=4 noet:
