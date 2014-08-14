29af04e9dec0ba00b522fe252c2d08130e680a03
Ivan Mincik <ivan.mincik@gmail.com>
2014-08-14 15:15:10 +0200

    server: managing connection forwarding as upstart job

--- system/server/115-connection-forwarding/install.sh
+++ system/server/115-connection-forwarding/install.sh
@@ -5,16 +5,20 @@
 
 
 # enable IP forwarding for client machines after each server start
-cat << EOF > /etc/rc.local
-#!/bin/sh -e
-$(gislab_config_header)
+cat << EOF > /etc/init/connection-forwarding.conf
+description "Internet connection forwarding"
 
+start on (net-device-up IFACE!=lo)
+stop on runlevel [06]
+
+script
+iptables --table nat --flush
 sysctl -w net.ipv4.ip_forward=1
 iptables --table nat --append POSTROUTING --jump MASQUERADE --source $GISLAB_NETWORK.0/24
-exit 0
+end script
 EOF
 
-sh /etc/rc.local
+service connection-forwarding restart
 
 
 # vim: set syntax=sh ts=4 sts=4 sw=4 noet:
