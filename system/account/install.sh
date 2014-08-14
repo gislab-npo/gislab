e8d74a92421e1aac88042459f8898cb43a5885dd
Ivan Mincik <ivan.mincik@gmail.com>
2014-08-14 16:58:51 +0200

    install: use more variables in 'account' install script

--- system/account/install.sh
+++ system/account/install.sh
@@ -8,25 +8,25 @@ mkdir /etc/skel/.local
 
 # applications menu
 mkdir -p /etc/skel/.config/menus
-cp $GISLAB_ROOT/system/account/desktop-session/menus/xfce-applications.menu /etc/skel/.config/menus/
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/menus/xfce-applications.menu /etc/skel/.config/menus/
 
 mkdir -p /etc/skel/.local/share/applications
-cp $GISLAB_ROOT/system/account/desktop-session/menus/*.desktop /etc/skel/.local/share/applications/
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/menus/*.desktop /etc/skel/.local/share/applications/
 
 mkdir -p /etc/skel/.local/share/desktop-directories
-cp $GISLAB_ROOT/system/account/desktop-session/menus/*.directory /etc/skel/.local/share/desktop-directories/
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/menus/*.directory /etc/skel/.local/share/desktop-directories/
 
 
 # GIS.lab desktop session and panel
 mkdir -p /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml
 
-cp $GISLAB_ROOT/system/account/desktop-session/xfce4/xinitrc /etc/skel/.config/xfce4/
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/xfce4/xinitrc /etc/skel/.config/xfce4/
 
-cp $GISLAB_ROOT/system/account/desktop-session/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
-cp $GISLAB_ROOT/system/account/desktop-session/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 
 mkdir -p /etc/skel/.config/xfce4/panel
-cp -a $GISLAB_ROOT/system/account/desktop-session/xfce4/panel/* /etc/skel/.config/xfce4/panel
+cp -a $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/xfce4/panel/* /etc/skel/.config/xfce4/panel
 
 # keyboard languages
 GISLAB_CLIENT_LANGUAGES="en, $GISLAB_CLIENT_LANGUAGES" # english language is alway available
@@ -41,28 +41,28 @@ sed -i "s/^variants=/variants=$variants/" /etc/skel/.config/xfce4/panel/xkb-plug
 
 # Conky
 mkdir -p /etc/skel/.config/autostart
-cp $GISLAB_ROOT/system/account/desktop-session/conky/conkyrc /etc/skel/.conkyrc
-cp $GISLAB_ROOT/system/account/desktop-session/conky/conky.desktop /etc/skel/.config/autostart/conky.desktop
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/conky/conkyrc /etc/skel/.conkyrc
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/desktop-session/conky/conky.desktop /etc/skel/.config/autostart/conky.desktop
 
 
 # Pidgin
 mkdir -p /etc/skel/.purple
-cp -a $GISLAB_ROOT/system/account/pidgin/*.xml /etc/skel/.purple
-#cp $GISLAB_ROOT/system/account/pidgin/pidgin.desktop /etc/skel/.config/autostart/pidgin.desktop
+cp -a $GISLAB_INSTALL_ACCOUNT_ROOT/pidgin/*.xml /etc/skel/.purple
+#cp $GISLAB_INSTALL_ACCOUNT_ROOT/pidgin/pidgin.desktop /etc/skel/.config/autostart/pidgin.desktop
 
 
 # PostgreSQL
-cp $GISLAB_ROOT/system/account/pgadmin3/pgadmin3 /etc/skel/.pgadmin3
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/pgadmin3/pgadmin3 /etc/skel/.pgadmin3
 
 
 # QGIS
 mkdir -p /etc/skel/.config/QGIS
 mkdir -p /etc/skel/.qgis2
-cp $GISLAB_ROOT/system/account/qgis/QGIS2.conf /etc/skel/.config/QGIS/QGIS2.conf
+cp $GISLAB_INSTALL_ACCOUNT_ROOT/qgis/QGIS2.conf /etc/skel/.config/QGIS/QGIS2.conf
 
 if [ "$GISLAB_CLIENT_GIS_DEVELOPMENT_SUPPORT" == "yes" ]; then
 	mkdir -p /etc/skel/bin
-	cp $GISLAB_ROOT/system/account/qgis/bin/gislab-dev-* /etc/skel/bin
+	cp $GISLAB_INSTALL_ACCOUNT_ROOT/qgis/bin/gislab-dev-* /etc/skel/bin
 	chmod 755 /etc/skel/bin/*
 fi
 
