604a1a2a5586bbb598ed886cb8968c41ffe6afec
Ivan Mincik <ivan.mincik@gmail.com>
2014-08-29 13:02:50 +0200

    Removing obsolete functions from 'functions.sh'

--- system/functions.sh
+++ system/functions.sh
@@ -2,39 +2,6 @@
 # UTILITY FUNCTIONS
 #
 
-gislab_config () {
-	# read default config file
-	source $GISLAB_ROOT/config.cfg
-	
-	if [ -f $GISLAB_ROOT/config-user.cfg ]
-	then
-		source $GISLAB_ROOT/config-user.cfg
-	fi
-}
-
-gislab_config_header () {
-	# print informative text about creation of configuration
-	# file by GIS.lab install
-	if [ -z "$1" ]; then
-		c="#"
-	else
-		c="$1"
-	fi
-
-	echo "${c} This file was created or modified by GIS.lab $GISLAB_VERSION on $(date)."
-}
-
-
-gislab_config_header_to_file () {
-	# write output of 'gislab_config_header' to first line of given file
-	if [ $# -eq 1 ]; then
-		sed -i -e "1i $(echo $(gislab_config_header)) " $1
-	elif [ $# -eq 2 ]; then
-		sed -i -e "1i $(echo $(gislab_config_header $1)) " $2
-	fi
-}
-
-
 gislab_print_info () {
 	# print informative message
 	echo -e "$(tput bold)[GIS.lab]: ${1}.$(tput sgr0)"
