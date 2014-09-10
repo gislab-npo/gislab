ae82ed53acb019a56bfd9b0353b43c60e31fb602
Ivan Mincik <ivan.mincik@gmail.com>
2014-09-10 13:16:48 +0200

    Install rewriting client plugins and re-enabling checking for missing ones

--- user/plugins/client/gislab-example-plugin.sh
+++ user/plugins/client/gislab-example-plugin.sh
@@ -1,34 +1,8 @@
+#!/bin/bash
 # Example GIS.lab client plugin
 
 
 echo "Running an example GIS.lab client plugin."
-case "$MODE" in
-	commandline)
-		add_option "example-option" "`eval_gettext "example option"`" "advanced" "true"
-	;;
 
-	configure)
-		if [ -n "$option_example_option_value" ]; then
-			EXAMPLE_OPTION="$(echo $option_example_option_value | tr ',' ' ')"
-		fi
-	;;
-
-	before-install)
-		echo "Performing before-install part of GIS.lab client installation."
-		echo "Example option: $EXAMPLE_OPTION"
-	;;
-
-	install)
-		echo "Performing install part of GIS.lab client installation."
-	;;
-
-	after-install)
-		echo "Performing after-install part of GIS.lab client installation."
-	;;
-
-	finalization)
-		echo "Performing finalization part of GIS.lab client installation."
-	;;
-esac
 
 # vim: set ts=4 sts=4 sw=4 noet:
