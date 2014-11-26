f21c3c3d115a7b15dc303bb1d6f6007313cfa22d
Ivan Mincik <ivan.mincik@gmail.com>
2014-11-26 09:54:34 +0100

    GIS.lab Web: moving 'user' page to URL 'web.gis.lab/user/<username>'

--- system/roles/service-gislab-web/files/static/gislab-web/webgis/viewer/urls.py
+++ system/roles/service-gislab-web/files/static/gislab-web/webgis/viewer/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns("webgis.viewer.views",
 	url(r"^$", "page", name="page"),
-	url(r"^share/(?P<username>[^/]*)/?$", "user_projects", name="user_projects"),
+	url(r"^user/(?P<username>[^/]*)/?$", "user_projects", name="user_projects"),
 	url("^owsrequest/$", "ows_request", name="owsrequest"),
 	url("^tile/(?P<project_hash>[^/]+)/(?P<publish>\d+)/tile/(?P<layers_hash>[^/]+)/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+)\.(?P<format>\w+)$", "tile", name="tile"),
 	url("^legend/(?P<project_hash>[^/]+)/(?P<publish>\d+)/legend/(?P<layer_hash>[^/]+)/(?P<zoom>\d+)\.(?P<format>\w+)$", "legend", name="legend"),
