ea023485429467aa5373a052a675430452b08f52
Ivan Mincik <ivan.mincik@gmail.com>
2015-02-04 20:55:03 +0100

    utils: moving QGIS compile and run scripts to utils

--- system/roles/account-users/files/static/system/install.sh
+++ system/roles/account-users/files/static/system/install.sh
@@ -44,12 +44,6 @@ mkdir -p /etc/skel/.qgis2
 cp $GISLAB_INSTALL_ACCOUNT_ROOT/qgis/QGIS2.conf /etc/skel/.config/QGIS/QGIS2.conf
 cp -a $GISLAB_INSTALL_ACCOUNT_ROOT/qgis/composer_templates /etc/skel/.qgis2/
 
-if [ "$GISLAB_CLIENT_GIS_DEVELOPMENT_SUPPORT" == "yes" ]; then
-	mkdir -p /etc/skel/bin
-	cp $GISLAB_INSTALL_ACCOUNT_ROOT/qgis/bin/gislab-dev-* /etc/skel/bin
-	chmod 755 /etc/skel/bin/*
-fi
-
 
 # final skeleton owner setting
 chown -R root:root /etc/skel
