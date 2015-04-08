465f5851cd6e8ee49b23d4e4f51790e0e630aa58
Ivan Mincik <ivan.mincik@gmail.com>
2015-04-08 06:16:06 +0200

    cluster: limit output from 'command' query to 512 bytes

--- system/roles/client-desktop/files/static/serf/command.sh
+++ system/roles/client-desktop/files/static/serf/command.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Run shell command with root privileges.
+# Run shell command with root privileges. Return last 512 bytes of response and exit code.
 
 COMMAND=$(cat)
-sh -c "$COMMAND" 2>&1 || true
+RESPONSE=$(sh -c "$COMMAND" 2>&1)
+EXIT=$?
+
+RESPONSE521=$(echo $RESPONSE | tail -c 512)
+echo "$RESPONSE521, EXIT CODE: $EXIT"
 
 
 # vim: set ts=4 sts=4 sw=4 noet:
