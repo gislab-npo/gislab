e59ba3fa8200c87438e71782e6ad5f59dd9a173e
Ivan Mincik <ivan.mincik@gmail.com>
2015-04-09 13:36:37 +0200

    cluster: fixed 'script' handler execution

--- system/roles/client-desktop/files/static/serf/script.sh
+++ system/roles/client-desktop/files/static/serf/script.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Run script located in 'Publish' directory with user privileges.
+# Run script located in 'Publish' directory with user privileges. Return last 512 bytes of response and exit code.
 
 PUBLISH_PATH=/mnt/publish
 SCRIPT_PATH=$(cat)
 SCRIPT=$PUBLISH_PATH/$SCRIPT_PATH
 
-USERNAME=$(echo $SCRIPT | awk -F '/' '{print $1}')
+USERNAME=$(echo $SCRIPT_PATH | awk -F '/' '{print $1}')
 
 
 if [ -f "$SCRIPT" ]; then
-	sudo -u $USERNAME sh "$SCRIPT" 2>&1 || true
+	RESPONSE=$(sudo -iu $USERNAME $SCRIPT 2>&1)
+	EXIT=$?
+
+	RESPONSE521=$(echo $RESPONSE | tail -c 512)
+	echo "$RESPONSE521, USERNAME: $USERNAME, EXIT CODE: $EXIT"
 else
-	echo "ERROR: Script '$SCRIPT' not found !"
+	echo "Script '$SCRIPT' not found !, EXIT CODE: 1"
+fi
 
 
 # vim: set ts=4 sts=4 sw=4 noet:
