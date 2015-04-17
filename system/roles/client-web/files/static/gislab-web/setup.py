5c5c6c4bbf7c1fdf54e1632a8755d2a470ebea0c
Ivan Mincik <ivan.mincik@gmail.com>
2015-04-18 00:01:40 +0200

    GIS.lab Web: deployment update

--- system/roles/client-web/files/static/gislab-web/setup.py
+++ system/roles/client-web/files/static/gislab-web/setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 
 # classifiers
 classifiers = [
 	'Development Status :: 4 - Beta',
-	"Framework :: Django",
+	'Environment :: Web Environment',
+	'Framework :: Django',
 	'Intended Audience :: Developers',
 	'Intended Audience :: Science/Research',
 	'License :: OSI Approved :: GNU General Public License version 3.0 (GPLv3)',
@@ -15,27 +17,9 @@ classifiers = [
 	'Topic :: Scientific/Engineering :: GIS',
 ]
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-package_root_dir = 'webgis'
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-	os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk(package_root_dir):
-	# Ignore dirnames that start with '.'
-	for i, dirname in enumerate(dirnames):
-		if dirname.startswith('.'): del dirnames[i]
-	if '__init__.py' in filenames:
-		pkg = dirpath.replace(os.path.sep, '.')
-		if os.path.altsep:
-			pkg = pkg.replace(os.path.altsep, '.')
-		packages.append(pkg)
-	elif filenames:
-		prefix = dirpath[len(package_root_dir)+1:] # Strip "package_root_dir/" from path
-		for f in filenames:
-			data_files.append(os.path.join(prefix, f))
+exclude_from_packages = [
+	'webgis.conf.project_template',
+]
 
 # setup
 setup(name='gislab-web',
@@ -44,9 +28,9 @@ setup(name='gislab-web',
 	author='Marcel Dancak, Ivan Mincik',
 	author_email='dancakm@gmail.com, ivan.mincik@gmail.com',
 	url='https://github.com/imincik/gis-lab/',
-	long_description=file('README.md','rb').read(),
-	package_dir={'gislab-web': '.'},
-	packages=packages,
-	package_data={'webgis': data_files},
+	packages=find_packages(),
+	include_package_data=True,
 	classifiers=classifiers
 )
+
+# vim: set ts=4 sts=4 sw=4 noet:
