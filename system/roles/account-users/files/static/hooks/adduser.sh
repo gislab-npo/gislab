281d7b267cab6a60bc576aa5e9fd81313ce1cca7
Ivan Mincik <ivan.mincik@gmail.com>
2015-05-20 14:46:41 +0200

    accounts: split GIS.lab account creation to allow automatic initialization on first login

---
+++ system/roles/account-users/files/static/hooks/adduser.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# GIS.lab user account hook for completing account creation once it is created in LDAP database.
+#
+# USAGE: adduser.sh <USERNAME>
+
+GISLAB_USER=$1
+
+# sanity check
+if [ "$(ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// "(uid=$GISLAB_USER)")" == "" ]; then
+	echo "User '$GISLAB_USER' doesn't exist in LDAP database !"
+	exit 1
+fi
+
+
+### HOME DIRECTORY
+# create home directory
+cp -pR /etc/skel /storage/home/$GISLAB_USER
+chown -R $GISLAB_USER:gislabusers /storage/home/$GISLAB_USER
+chmod 0700 /storage/home/$GISLAB_USER
+
+# replace user name placeholder with user name of created user
+find /storage/home/$GISLAB_USER -type f -exec sed -i "s/{+ GISLAB_USER +}/$GISLAB_USER/g" "{}" \;
+
+# create ~/.gislab directory
+mkdir -p /storage/home/$GISLAB_USER/.gislab
+chmod 700 /storage/home/$GISLAB_USER/.gislab
+chown $GISLAB_USER:gislabusers /storage/home/$GISLAB_USER/.gislab
+
+
+### POSTGRESQL
+# create PostgreSQL user account
+createuser -U postgres --no-superuser --no-createdb --no-createrole $GISLAB_USER
+psql -U postgres -c "GRANT gislabusers TO $GISLAB_USER;"
+psql -U postgres -d gislab -c "CREATE SCHEMA AUTHORIZATION $GISLAB_USER;"
+
+# add user to the database superusers group if creating superuser account
+id gislab | grep gislabadmins &> /dev/null && SUDO=yes || SUDO=no
+if [ "$SUDO" == "yes" ]; then
+	psql -U postgres -c "GRANT gislabadmins TO $GISLAB_USER;"
+fi
+
+
+### PUBLISH DIRECTORY
+# create publish directory
+mkdir -p /storage/publish/$GISLAB_USER # NFS directory
+chown $GISLAB_USER:www-data /storage/publish/$GISLAB_USER
+chmod 750 /storage/publish/$GISLAB_USER
+
+
+### VPN
+# place VPN certificates to ~/.gislab directory
+if [ -d "/etc/openvpn" ]; then
+	cp /etc/openvpn/gislab_vpn_ca.crt /storage/home/$GISLAB_USER/.gislab/gislab_vpn_ca.crt
+	chown $GISLAB_USER:gislabusers /storage/home/$GISLAB_USER/.gislab/gislab_vpn_ca.crt
+
+	cp /etc/openvpn/gislab_vpn_ta.key /storage/home/$GISLAB_USER/.gislab/gislab_vpn_ta.key
+	chown $GISLAB_USER:gislabusers /storage/home/$GISLAB_USER/.gislab/gislab_vpn_ta.key
+	chmod 0600 /storage/home/$GISLAB_USER/.gislab/gislab_vpn_ta.key
+fi
+
+
+### DONE
+echo "$(date +%Y-%m-%d-%H:%M:%S)" > /storage/home/$GISLAB_USER/.gislab/account.done
+
+
+# vim: set ts=4 sts=4 sw=4 noet:
