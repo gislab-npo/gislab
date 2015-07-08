862d2e9af5ff6aba12b0b755e98ee56d8164c37a
Ivan Mincik <ivan.mincik@gmail.com>
2015-07-08 12:46:30 +0200

    utils: adding script for waking up LAN machines by MAC

---
+++ utils/wake-machines.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Wake machines in LAN with given MAC addresses (require etherwake).
+# USAGE: $(basename $0) MAC,MAC,...
+
+
+IFS=', ' read -a maclist <<< "$1"
+for mac in "${maclist[@]}"; do
+	sudo etherwake -D $mac
+done
+
+
+# vim: set ts=4 sts=4 sw=4 noet:
