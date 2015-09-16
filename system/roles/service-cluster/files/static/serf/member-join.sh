074028d7e644975be2363e2480f975a59d53b9e6
Ivan Mincik <ivan.mincik@gmail.com>
2015-09-16 21:45:04 +0200

    cluster: detaching Munin monitoring from Serf handlers

--- system/roles/service-cluster/files/static/serf/member-join.sh
+++ system/roles/service-cluster/files/static/serf/member-join.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
 # Perform actions when GIS.lab cluster join event is received:
-#  * activate statistics monitoring (it will stay activated even if machine leaves cluster)
+#  * nothing to do now
 
-
-while read line; do
-	NAME=`echo $line | awk '{print \$1 }'`
-	ADDRESS=`echo $line | awk '{print \$2 }'`
-	ROLE=`echo $line | awk '{print \$3 }'`
-
-	# do nothing if event was sent by GIS.lab server
-	if [ "${ROLE}" == "server" ]; then
-		continue
-	fi
-	
-	# activate statistics monitoring
-	echo -e "[gis.lab;$NAME] # Managed by Serf\n  address $ADDRESS\n  use_node_name yes\n" > /etc/munin/munin-conf.d/$NAME-gislab.conf
-
-done
-
-service munin-node restart
-
+exit 0
 
 # vim: set ts=4 sts=4 sw=4 noet:
