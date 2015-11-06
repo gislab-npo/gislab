e511301170ccead9e66dbaea7c21189dc20a1716
Jachym Cepicky <jachym.cepicky@gmail.com>
2015-11-06 16:43:36 +0100

    started to work on server-side filtering

--- system/roles/client-web/files/static/gislab-web/webgis/viewer/urls.py
+++ system/roles/client-web/files/static/gislab-web/webgis/viewer/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns("webgis.viewer.views",
 	url(r"^$", "app", name="app"),
+	url(r"^filter/$", "filter", name="filter"),
 )
--- system/roles/client-web/files/static/gislab-web/webgis/viewer/views.py
+++ system/roles/client-web/files/static/gislab-web/webgis/viewer/views.py
@@ -8,6 +8,8 @@ from django.http import HttpResponse, Http404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+#from owslib.wfs import WebFeatureService
+
 
 def app(request):
 	data = {
@@ -19,3 +21,11 @@ def app(request):
 		data,
 		content_type="text/html"
 	)
+
+def filter(request):
+    url = settings.GISLAB_WEB_MAPSERVER_URL
+    path = settings.GISLAB_WEB_PROJECT_ROOT
+    project = request.GET['PROJECT']
+    print url, project
+    print url + "?MAP=" + path + project + "&service=wms&request=getcapabilities"
+    return HttpResponse('{"Ahoj":"svete"}', content_type="application/json")
