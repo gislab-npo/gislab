60c34625cd2f62953933521a5183958a71946843
Ivan Mincik <ivan.mincik@gmail.com>
2015-11-19 10:09:32 +0100

    utils: adding scripts for running or testing separate ansible roles

---
+++ utils/vagrant-roles-test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Test only specified GIS.lab roles.
+# USAGE: vagrant-roles-test.sh <role-name>
+
+
+PYTHONUNBUFFERED=1
+ANSIBLE_FORCE_COLOR=true
+ANSIBLE_HOST_KEY_CHECKING=false
+ANSIBLE_SSH_ARGS='\
+    -o UserKnownHostsFile=/dev/null \
+    -o ForwardAgent=yes \
+    -o ControlMaster=auto \
+    -o ControlPersist=60s'
+
+ansible-playbook \
+  --private-key=$(pwd)/.vagrant/machines/gislab_vagrant/virtualbox/private_key \
+  --user=vagrant \
+  --connection=ssh \
+  --limit='gislab_vagrant' \
+  --inventory-file=$(pwd)/.vagrant/provisioners/ansible/inventory \
+  --extra-vars='{"GISLAB_ADMIN_PASSWORD":"gislab","GISLAB_SERVER_NETWORK_DEVICE":"eth1"}' \
+  --verbose \
+  --tags installation-setup-test,$@ \
+system/test.yml
+
+# vim: set ts=8 sts=4 sw=4 et:
