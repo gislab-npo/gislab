c69183a7ae75ed41a39373dd506e3b6a30ddb637
Ivan Mincik <ivan.mincik@gmail.com>
2015-11-20 22:45:48 +0100

    Code cleanup - admin tools

--- system/roles/account-users/files/static/hooks/adduser.sh
+++ system/roles/account-users/files/static/hooks/adduser.sh
@@ -8,22 +8,24 @@ source /etc/gislab_version
 source $GISLAB_ROOT/system/functions.sh
 
 
-# read GISLAB_USER from script parameter if given
+### OPTIONS
 if [ "$1" != "" ]; then
     GISLAB_USER=$1
 fi
 
 
+### VARIABLES
 lds="ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:///"
 
-# sanity check
+
+### SANITY CHECKS
 if [ "$($lds "(uid=$GISLAB_USER)")" == "" ]; then
     echo "User '$GISLAB_USER' doesn't exist in LDAP database !"
-    exit 1
 fi
 
 
-### HOME DIRECTORY
+### MAIN SCRIPT
+# HOME DIRECTORY
 # create home directory
 cp -pR $GISLAB_PATH_SYSTEM/accounts/skel $GISLAB_PATH_HOME/$GISLAB_USER
 
@@ -37,18 +39,30 @@ chmod 0700 $GISLAB_PATH_HOME/$GISLAB_USER
 find $GISLAB_PATH_HOME/$GISLAB_USER \
     -type f \
     -exec sed -i "s/{+ GISLAB_USER +}/$GISLAB_USER/g" "{}" \;
+
 find $GISLAB_PATH_HOME/$GISLAB_USER \
     -type f \
     -exec sed -i "s/{+ GISLAB_USER_GIVEN_NAME +}/$GISLAB_USER_GIVEN_NAME/g" "{}" \;
+
 find $GISLAB_PATH_HOME/$GISLAB_USER \
     -type f \
     -exec sed -i "s/{+ GISLAB_USER_SURNAME +}/$GISLAB_USER_SURNAME/g" "{}" \;
+
 find $GISLAB_PATH_HOME/$GISLAB_USER \
     -type f \
     -exec sed -i "s/{+ GISLAB_USER_EMAIL +}/$GISLAB_USER_EMAIL/g" "{}" \;
+
 find $GISLAB_PATH_HOME/$GISLAB_USER \
     -type f \
     -exec sed -i "s/{+ GISLAB_USER_DESCRIPTION +}/$GISLAB_USER_DESCRIPTION/g" "{}" \;
+
+find $GISLAB_PATH_HOME/$GISLAB_USER \
+    -type f \
+    -exec sed -i "s/{+ GISLAB_USER_SUPERUSER +}/$GISLAB_USER_SUPERUSER/g" "{}" \;
+
+find $GISLAB_PATH_HOME/$GISLAB_USER \
+    -type f \
+    -exec sed -i "s/{+ GISLAB_USER_GROUPS +}/$GISLAB_USER_GROUPS/g" "{}" \;
 
 # create ~/.gislab directory
 mkdir -p $GISLAB_PATH_HOME/$GISLAB_USER/.gislab
@@ -56,27 +70,27 @@ chmod 700 $GISLAB_PATH_HOME/$GISLAB_USER/.gislab
 chown $GISLAB_USER:gislabusers $GISLAB_PATH_HOME/$GISLAB_USER/.gislab
 
 
-### POSTGRESQL
+# POSTGRESQL
 # create PostgreSQL user account
 createuser -U postgres --no-superuser --no-createdb --no-createrole $GISLAB_USER
 psql -U postgres -c "GRANT gislabusers TO $GISLAB_USER;"
 psql -U postgres -d gislab -c "CREATE SCHEMA AUTHORIZATION $GISLAB_USER;"
 
 # add user to the database superusers group if creating superuser account
-id $GISLAB_USER | grep gislabadmins &> /dev/null && SUDO=yes || SUDO=no
-if [ "$SUDO" == "yes" ]; then
+id $GISLAB_USER | grep gislabadmins &> /dev/null && sudo=yes || sudo=no
+if [ "$sudo" == "yes" ]; then
     psql -U postgres -c "GRANT gislabadmins TO $GISLAB_USER;"
 fi
 
 
-### PUBLISH DIRECTORY
+# PUBLISH DIRECTORY
 # create publish directory
 mkdir -p /storage/publish/$GISLAB_USER # NFS directory
 chown $GISLAB_USER:www-data /storage/publish/$GISLAB_USER
 chmod 750 /storage/publish/$GISLAB_USER
 
 
-### VPN
+# VPN
 # place VPN configuration and certificates to ~/.gislab directory
 if [ -d "/etc/openvpn" ]; then
     mkdir -p $GISLAB_PATH_HOME/$GISLAB_USER/.gislab
@@ -91,6 +105,7 @@ if [ -d "/etc/openvpn" ]; then
     chown \
         $GISLAB_USER:gislabusers \
         $GISLAB_PATH_HOME/$GISLAB_USER/.gislab/$GISLAB_UNIQUE_ID-vpn.tar.gz
+
     chmod \
         0600 \
         $GISLAB_PATH_HOME/$GISLAB_USER/.gislab/$GISLAB_UNIQUE_ID-vpn.tar.gz
--- system/roles/account-users/files/static/hooks/deluser.sh
+++ system/roles/account-users/files/static/hooks/deluser.sh
@@ -8,33 +8,37 @@ source /etc/gislab_version
 source $GISLAB_ROOT/system/functions.sh
 
 
-# read GISLAB_USER from script parameter if given
+### OPTIONS
 if [ "$1" != "" ]; then
     GISLAB_USER=$1
 fi
 
 
+### VARIABLES
 lds="ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:///"
 
-# sanity check
+
+### SANITY CHECKS
 if [ "$($lds "(uid=$GISLAB_USER)")" != "" ]; then
     echo "User '$GISLAB_USER' still exist in LDAP database !"
-    exit 1
 fi
 
 
-### HOME DIRECTORY
+### MAIN SCRIPT
+# HOME DIRECTORY
 # remove home directory
 rm -rf $GISLAB_PATH_HOME/$GISLAB_USER
 
 
-### POSTGRESQL
+# POSTGRESQL
 # drop database schema
 psql -U postgres -d gislab -c "DROP SCHEMA $GISLAB_USER CASCADE"
 psql -U postgres -d gislab -c "DROP OWNED BY $GISLAB_USER CASCADE"
 
 # delete data from GIS.lab Web client if installed
-ball_exist_sql="SELECT 1 FROM information_schema.tables WHERE table_name = 'storage_ball'"
+ball_exist_sql="SELECT 1 FROM information_schema.tables
+                    WHERE table_name = 'storage_ball'"
+
 if [[ $(psql -U postgres -d gislab -tAc "$ball_exist_sql") == "1" ]]; then
     psql -U postgres -d gislab \
         -c "DELETE FROM storage_drawing WHERE \"user\" = '$GISLAB_USER'"
@@ -46,7 +50,7 @@ fi
 dropuser -U postgres $GISLAB_USER
 
 
-### PUBLISH DIRECTORY
+# PUBLISH DIRECTORY
 rm -rf /storage/publish/$GISLAB_USER
 
 # vim: set ts=8 sts=4 sw=4 et:
