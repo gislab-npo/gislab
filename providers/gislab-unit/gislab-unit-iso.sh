e0f7b003445b6f9b04e3243ef26bff21ce56d373
Ivan Mincik <ivan.mincik@gmail.com>
2015-11-22 16:48:56 +0100

    providers/gislab-unit: code cleanup

--- providers/gislab-unit/gislab-unit-iso.sh
+++ providers/gislab-unit/gislab-unit-iso.sh
@@ -2,63 +2,82 @@
 
 set -e
 
-usage () {
-	echo
-	echo "Create GIS.lab base system installation ISO image from Ubuntu Server ISO."
-	echo "Script must be executed with superuser privileges."
-	echo
-	echo "USAGE: $(basename $0) -s <country code> -t <timezone> -k <SSH public key>"
-	echo "                          -w <working directory> -i <ISO image>"
-	echo
-	echo "  -s country code used for choosing closest repository mirror (e.g. SK)"
-	echo "  -t timezone (e.g. Europe/Bratislava)"
-	echo "  -k SSH public key file, which will be used for GIS.lab installation or update"
-	echo "  -w working directory with enough disk space (2.5 x larger than ISO image size)"
-	echo "  -i Ubuntu Server installation ISO image file"
-	echo "  -h this help"
-	echo
-	exit 1
+
+### USAGE
+function usage() {
+    echo "USAGE: $(basename $0) [OPTIONS]"
+    echo "Create GIS.lab base system installation ISO image from Ubuntu Server ISO."
+    echo "Script must be executed with superuser privileges."
+    echo -e "\nOPTIONS
+    -s country code used for choosing closest repository mirror (e.g. SK)
+    -t timezone (e.g. Europe/Bratislava)
+    -k SSH public key file, which will be used for GIS.lab installation or update
+    -w working directory with enough disk space (2.5 x larger than ISO image size)
+    -i Ubuntu Server installation ISO image file
+    -h display this help
+    "
+    exit 255
 }
 
-clean_up () {
-	mount | grep -q " $MOUNT_DIR " && sudo umount $MOUNT_DIR && rm -rf $MOUNT_DIR
-	test -d $WORK_DIR && rm -rf $WORK_DIR
-}
-	
 
+### OPTIONS
 while getopts "s:t:i:w:k:h" OPTION; do
-
-	case "$OPTION" in
-		s) COUNTRY_CODE="$OPTARG" ;;
-		t) TIME_ZONE="$OPTARG" ;;
-		k) SSH_PUBLIC_KEY="$OPTARG" ;;
-		w) WORK_DIR="$OPTARG"
-		   ROOT_DIR="$WORK_DIR/root" ;;
-		i) SRC_IMAGE="$OPTARG" ;;
-		h) usage ;;
-		\?) usage ;;
-	esac
+    case "$OPTION" in
+        s) COUNTRY_CODE="$OPTARG" ;;
+        t) TIME_ZONE="$OPTARG" ;;
+        k) SSH_PUBLIC_KEY="$OPTARG" ;;
+        w) WORK_DIR="$OPTARG"
+           ROOT_DIR="$WORK_DIR/root" ;;
+        i) SRC_IMAGE="$OPTARG" ;;
+        h) usage ;;
+        \?) usage ;;
+    esac
 done
 
 
-# sanity checks
-if [ -z "$COUNTRY_CODE" -o -z "$TIME_ZONE" -o -z "$SRC_IMAGE" -o -z "$WORK_DIR" -o -z "$SSH_PUBLIC_KEY" ]; then
-	usage
-fi
-
-if [ $(id -u) -ne 0 ]; then
-	echo "This command can be run only with superuser privileges"
-	exit 1
-fi
-
-
+### VARIABLES
 SRC_DIR="$(dirname $(readlink -f $0))"
 MOUNT_DIR="/tmp/gislab-base-system-iso-mnt"
 ISO_ID=$(pwgen -n 8 1)
 DATE=$(date '+%Y-%m-%d-%H:%M:%S')
 
 
+### SANITY CHECKS
+if [ -z "$COUNTRY_CODE" \
+    -o -z "$TIME_ZONE" \
+    -o -z "$SRC_IMAGE" \
+    -o -z "$WORK_DIR" \
+    -o -z "$SSH_PUBLIC_KEY" ]; then
+    usage
+fi
+
+if [ $(id -u) -ne 0 ]; then
+    echo "This command can be run only with superuser privileges"
+    exit 1
+fi
+
+# mount ISO image, check and keep it for rest of the script
 mkdir -p $MOUNT_DIR
+sudo mount -o loop $SRC_IMAGE $MOUNT_DIR
+
+if [ ! -f "$MOUNT_DIR/install/vmlinuz" ]; then
+    echo "Invalid Ubuntu ISO image file. Ubuntu 12.04 Server ISO is required."
+    umount $MOUNT_DIR
+    exit 1
+fi
+
+
+### FUNCTIONS
+clean_up () {
+    mount \
+        | grep -q " $MOUNT_DIR " \
+        && sudo umount $MOUNT_DIR \
+        && rm -rf $MOUNT_DIR
+    test -d $WORK_DIR && rm -rf $WORK_DIR
+}
+
+
+### MAIN SCRIPT
 mkdir -p $WORK_DIR
 mkdir -p $ROOT_DIR
 
@@ -67,16 +86,7 @@ mkdir -p $ROOT_DIR
 trap clean_up SIGHUP SIGINT SIGKILL
 
 
-# load original ISO image content
-sudo mount -o loop $SRC_IMAGE $MOUNT_DIR
-
-# check if using valid Ubuntu ISO image file
-if [ ! -f "$MOUNT_DIR/install/vmlinuz" ]; then
-	echo "Invalid Ubuntu ISO image file. Ubuntu 12.04 Server ISO is required."
-	umount $MOUNT_DIR
-	exit 1
-fi
-
+# copy ISO image content and umount
 rsync -a $MOUNT_DIR/ $ROOT_DIR/
 umount $MOUNT_DIR
 
@@ -85,19 +95,19 @@ cd $ROOT_DIR
 
 # boot options
 sed -i 's/^timeout.*/timeout 50/' $ROOT_DIR/isolinux/isolinux.cfg
-cp -f $SRC_DIR/preseed/menu.cfg $ROOT_DIR/isolinux/menu.cfg
-cp -f $SRC_DIR/preseed/txt.cfg $ROOT_DIR/isolinux/txt.cfg
-cp -f $SRC_DIR/preseed/splash.pcx $ROOT_DIR/isolinux/splash.pcx
+cp -f $SRC_DIR/iso/menu.cfg $ROOT_DIR/isolinux/menu.cfg
+cp -f $SRC_DIR/iso/txt.cfg $ROOT_DIR/isolinux/txt.cfg
+cp -f $SRC_DIR/iso/splash.pcx $ROOT_DIR/isolinux/splash.pcx
 
 
 # generate preseed file
-cp $SRC_DIR/preseed/gislab.seed.template $ROOT_DIR/preseed/gislab.seed
+cp $SRC_DIR/iso/gislab.seed.template $ROOT_DIR/preseed/gislab.seed
 sed -i "s;###COUNTRY_CODE###;$COUNTRY_CODE;" $ROOT_DIR/preseed/gislab.seed
 sed -i "s;###TIME_ZONE###;$TIME_ZONE;" $ROOT_DIR/preseed/gislab.seed
 
 cp $SSH_PUBLIC_KEY $ROOT_DIR/ssh_key.pub
 
-cp $SRC_DIR/preseed/configure-apt-proxy.sh $ROOT_DIR/configure-apt-proxy.sh
+cp $SRC_DIR/iso/configure-apt-proxy.sh $ROOT_DIR/configure-apt-proxy.sh
 chmod 0755 $ROOT_DIR/configure-apt-proxy.sh
 
 
@@ -109,18 +119,23 @@ rm -f $ROOT_DIR/isolinux/boot.cat
 
 # update md5sum file
 rm -f $ROOT_DIR/md5sum.txt
-find -type f -print0 | xargs -0 md5sum | grep -v 'isolinux/boot.cat' > $ROOT_DIR/md5sum.txt
+find -type f -print0 \
+    | xargs -0 md5sum \
+    | grep -v 'isolinux/boot.cat' > $ROOT_DIR/md5sum.txt
 
 cd $WORK_DIR
 
-# create output ISO image file 
+# create output ISO image file
 #genisoimage -o gislab-base-system.iso -b isolinux/isolinux.bin \
 #            -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
 #            -boot-info-table -iso-level 2 -r root/
 
-mkisofs -D -r -V "GIS.lab Base System" -cache-inodes -J -l -b isolinux/isolinux.bin \
-	-c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table \
-	-o gislab-base-system-${ISO_ID}.iso root/
+mkisofs -D -r \
+    -V "GIS.lab Base System" \
+    -cache-inodes \
+    -J -l -b isolinux/isolinux.bin \
+    -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table \
+    -o gislab-base-system-${ISO_ID}.iso root/
 
 # create meta file
 cat << EOF >> $WORK_DIR/gislab-base-system-${ISO_ID}.meta
@@ -136,8 +151,10 @@ rm -rf $MOUNT_DIR
 rm -rf $ROOT_DIR
 
 
-# done
+### DONE
 echo
 echo "GIS.lab Base System ISO: $WORK_DIR/gislab-base-system-${ISO_ID}.iso"
 echo "GIS.lab Base System ISO meta:  $WORK_DIR/gislab-base-system-${ISO_ID}.meta"
 echo
+
+# vim: set ts=8 sts=4 sw=4 et:
