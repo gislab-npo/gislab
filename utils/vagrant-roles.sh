9cc806863d142eac9675e1da9b6713eadfe41f4a
Ivan Mincik <ivan.mincik@gmail.com>
2015-11-23 15:31:14 +0100

    utils: vagrant.roles.sh - one tool for running separate roles with tests

--- utils/vagrant-roles.sh
+++ utils/vagrant-roles.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
-# Run only specified GIS.lab roles.
-# USAGE: vagrant-roles.sh <role-name>
+
+### USAGE
+function usage() {
+    echo "USAGE: $(basename $0) [OPTIONS] <role-name>,<role-name>,..."
+    echo "Run requested GIS.lab roles only."
+    echo -e "\nOPTIONS
+    -t run only tests for requested roles (useful for tests development)
+    -h display this help
+    "
+    exit 255
+}
 
 
+### OPTIONS
+opt_tests_only="no"
+
+while getopts "t" OPTION
+do
+        case "$OPTION" in
+            t) opt_tests_only="yes" ;;
+            h) usage ;;
+            \?) exit 1;;
+        esac
+done
+shift $(($OPTIND - 1))
+if [ $# -eq 0 ]; then usage; fi
+
+ROLES=$1
+
+
+### VARIABLES
+ansible_cmd="ansible-playbook \
+  --private-key=$(pwd)/.vagrant/machines/gislab_vagrant/virtualbox/private_key \
+  --user=vagrant \
+  --connection=ssh \
+  --limit=gislab_vagrant \
+  --inventory-file=$(pwd)/.vagrant/provisioners/ansible/inventory \
+  --extra-vars={\"GISLAB_ADMIN_PASSWORD\":\"gislab\",\"GISLAB_SERVER_NETWORK_DEVICE\":\"eth1\"} \
+  --verbose"
+
+
+### MAIN SCRIPT
 PYTHONUNBUFFERED=1
 ANSIBLE_FORCE_COLOR=true
 ANSIBLE_HOST_KEY_CHECKING=false
@@ -12,15 +50,12 @@ ANSIBLE_SSH_ARGS='\
     -o ControlMaster=auto \
     -o ControlPersist=60s'
 
-ansible-playbook \
-  --private-key=$(pwd)/.vagrant/machines/gislab_vagrant/virtualbox/private_key \
-  --user=vagrant \
-  --connection=ssh \
-  --limit='gislab_vagrant' \
-  --inventory-file=$(pwd)/.vagrant/provisioners/ansible/inventory \
-  --extra-vars='{"GISLAB_ADMIN_PASSWORD":"gislab","GISLAB_SERVER_NETWORK_DEVICE":"eth1"}' \
-  --verbose \
-  --tags installation-setup,$@ \
-system/gislab.yml
+# run roles
+if [ "$opt_tests_only" == "no" ]; then
+    $ansible_cmd --tags installation-setup,$ROLES system/gislab.yml
+fi
+
+# run tests for roles
+$ansible_cmd --tags installation-setup,$ROLES system/test.yml
 
 # vim: set ts=8 sts=4 sw=4 et:
