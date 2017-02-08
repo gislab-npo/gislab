345e58a1adb754a389ab47dc88da3c4d69690532
Matej Pastor <matej.pastor@initipi.sk>
2017-02-08 14:07:37 +0100

    service-statistics: dynamic update configuration

--- system/roles/service-cluster/files/static/serf/member-join.sh
+++ system/roles/service-cluster/files/static/serf/member-join.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
 # Perform actions when GIS.lab cluster join event is received:
-#  * nothing to do now
+#  * update statictics system configuration
 
-exit 0
+GISLAB_CLIENTS=$(serf members -tag role=client -status=alive | sed 's/^\(.\+\):.\+$/\1/' | sort -n)
+
+echo "contact.admins.command mail -s \"[GIS.lab ID: gislab_vagrant] \${var:graph_title}\" root
+
+[gis.lab;]
+
+[gis.lab;server]
+    address 127.0.0.1
+    use_node_name yes
+" >/etc/munin/munin.conf
+
+OLD_IFS=$IFS
+IFS='
+'
+
+for CLIENT in $GISLAB_CLIENTS; do
+    HOST=$(echo $CLIENT | awk '{ print $1 }')
+    IP=$(echo $CLIENT | awk '{ print $2 }')
+
+    echo "[gis.lab;$HOST.gis.lab]
+    address $IP
+    use_node_name yes
+" >> /etc/munin/munin.conf
+done
+
+IFS=$OLD_IFS
 
 # vim: set ts=8 sts=4 sw=4 et:
