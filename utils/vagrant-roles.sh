b3a8fb784e30a09cfa525d54f796f8f27f7f3e65
Martin Landa <landa.martin@gmail.com>
2018-10-28 17:47:01 +0100

    vagrant-roles.sh: fix VM name (gislab_vagrant_xenial)

--- utils/vagrant-roles.sh
+++ utils/vagrant-roles.sh
@@ -38,10 +38,10 @@ ROLES=$1
 
 ### VARIABLES
 ansible_cmd="ansible-playbook \
-  --private-key=$(pwd)/.vagrant/machines/gislab_vagrant_bionic/virtualbox/private_key \
+  --private-key=$(pwd)/.vagrant/machines/gislab_vagrant_xenial/virtualbox/private_key \
   --user=vagrant \
   --connection=ssh \
-  --limit=gislab_vagrant_bionic \
+  --limit=gislab_vagrant_xenial \
   --inventory-file=$(pwd)/.vagrant/provisioners/ansible/inventory \
   --verbose"
 
